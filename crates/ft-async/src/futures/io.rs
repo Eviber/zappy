5b50f8a3f4b3abb896a4f7321f0af08ad299563b
nils-mathieu <nils.mathieu.contact@gmail.com>
2023-11-11 18:00:06 +0100

    initial basic handshake implementation

--- crates/ft-async/src/futures/io.rs
+++ crates/ft-async/src/futures/io.rs
@@ -2,12 +2,29 @@ use core::future::Future;
 use core::pin::Pin;
 use core::task::{Context, Poll};
 
+use ft::collections::ReadBuffer;
+
 use crate::EXECUTOR;
 
 /// Returns a future that completes when the `buf` has been completely written to the
 /// provided file descriptor.
 pub fn write_all(fd: ft::Fd, buf: &[u8]) -> WriteAll {
     WriteAll { fd, buf }
+}
+
+/// Returns a future that completes when a complete line (delimited by `\n`) has been read
+/// from the provided file descriptor.
+///
+/// # Remarks
+///
+/// If the end of file is reached before the end of a line, the future will complete with
+/// an error (`ft::Errno::CONNECTION_RESET`).
+///
+/// # Returns
+///
+/// An error, or the line without the final delimiter.
+pub fn read_line(fd: ft::Fd, buf: &mut ReadBuffer) -> ReadLine {
+    ReadLine { fd, buf }
 }
 
 /// See [`write_all`].
@@ -33,6 +50,50 @@ impl<'a> Future for WriteAll<'a> {
         } else {
             EXECUTOR.wake_me_up_on_write(self.fd, cx.waker().clone());
             Poll::Pending
+        }
+    }
+}
+
+/// See [`read_line`].
+pub struct ReadLine<'a> {
+    fd: ft::Fd,
+    buf: &'a mut ReadBuffer,
+}
+
+impl<'a> Future for ReadLine<'a> {
+    type Output = ft::Result<&'a [u8]>;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
+        let fd = self.fd;
+
+        // Make sure that the buffer has enough space to read at least 64 bytes.
+        match self.buf.reserve(64) {
+            Ok(()) => (),
+            Err(err) => return Poll::Ready(Err(err.into())),
+        }
+
+        // Try to read from the file descriptor.
+        let added = match self.buf.fill_with_fd(fd) {
+            Ok([]) => return Poll::Ready(Err(ft::Errno::CONNECTION_RESET)),
+            Ok(added) => added,
+            Err(err) => return Poll::Ready(Err(err)),
+        };
+
+        // Try to find the index of the delimiter.
+        let Some(index) = added.iter().position(|&byte| byte == b'\n') else {
+            EXECUTOR.wake_me_up_on_read(self.fd, cx.waker().clone());
+            return Poll::Pending;
+        };
+
+        // Consume and return the line.
+
+        // SAFETY:
+        //  `index + 1` is at most `added.len()` which ensures that we won't overflow
+        //  the size of the pending block in the buffer.
+        unsafe {
+            let consumed = self.buf.pending().as_ptr();
+            self.buf.consume_unchecked(index);
+            Poll::Ready(Ok(core::slice::from_raw_parts(consumed, index)))
         }
     }
 }
