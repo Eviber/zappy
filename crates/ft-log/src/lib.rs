9936f3577e4f2d7ec8401354b4da703852b780a4
nils-mathieu <nils.mathieu.contact@gmail.com>
2023-11-11 16:27:52 +0100

    add lints to all crates

--- crates/ft-log/src/lib.rs
+++ crates/ft-log/src/lib.rs
@@ -1,6 +1,8 @@
 //! A simple logging facade.
 
 #![no_std]
+#![deny(clippy::unwrap_used, unsafe_op_in_unsafe_fn)]
+#![warn(missing_docs, clippy::must_use_candidate)]
 
 use core::fmt::Arguments;
 use core::sync::atomic::AtomicU8;
