eaa2f1d3e0073360e94e1df91ab4f68ad09e1761
nmathieu <nmathieu@student.42.fr>
2023-11-11 15:31:08 +0100

    initial implementation of the logger

---
+++ crates/ft-log/src/lib.rs
@@ -0,0 +1,147 @@
+//! A simple logging facade.
+
+#![no_std]
+
+use core::fmt::Arguments;
+use core::sync::atomic::AtomicU8;
+use core::sync::atomic::Ordering::Relaxed;
+
+use Verbosity::*;
+
+/// A logging verbosity level.
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Verbosity {
+    /// The message is simply tracing some event in the program. This is generally not
+    /// useful for the end user.
+    Trace = 1,
+    /// The message provides some insight about the current stae of the program. This is
+    /// generally useful to display, but not necessary.
+    Info = 2,
+    /// A debug message.
+    ///
+    /// Messages with this verbosity level should generally not stay in the code for
+    /// long.
+    Debug = 4,
+    /// A warning message. Messages with this verbosity level should generally be displayed
+    /// to the user, but they are not critical.
+    Warning = 8,
+    /// An error message. Messages with this verbosity level should always be displayed to
+    /// the user.
+    Error = 16,
+}
+
+/// A logging verbosity filter.
+pub struct VerbosityFilter(AtomicU8);
+
+impl VerbosityFilter {
+    /// Creates a new [`VerbosityFilter`] that allows all messages.
+    const fn new() -> Self {
+        Self(AtomicU8::new(0xFF))
+    }
+
+    /// Inserts a new verbosity level into the filter.
+    #[inline]
+    pub fn insert(&self, level: Verbosity) {
+        self.0.fetch_or(level as u8, Relaxed);
+    }
+
+    /// Removes a verbosity level from the filter.
+    #[inline]
+    pub fn remove(&self, level: Verbosity) {
+        self.0.fetch_and(!(level as u8), Relaxed);
+    }
+
+    /// Returns whether the filter contains the provided verbosity level.
+    pub fn contains(&self, level: Verbosity) -> bool {
+        self.0.load(Relaxed) & (level as u8) != 0
+    }
+}
+
+/// A message to log to the console.
+pub struct Message<'a> {
+    /// The verbosity level of the message to log.
+    pub verbosity: Verbosity,
+    /// The message to write.
+    pub message: Arguments<'a>,
+}
+
+/// Creates a new [`Message`] at the provided verbosity level.
+///
+/// The message is created with provenance information for the location of the
+/// macro invocation.
+#[macro_export]
+macro_rules! message {
+    ($v:expr, $($args:tt)*) => {
+        $crate::Message {
+            verbosity: $v,
+            message: ::core::format_args!($($args)*),
+        }
+    };
+}
+
+/// The verbosity level filter for all messages.
+pub static VERBOSITY: VerbosityFilter = VerbosityFilter::new();
+
+/// Logs the provided message.
+#[inline]
+pub fn log(message: &Message) {
+    if VERBOSITY.contains(message.verbosity) {
+        log_unchecked(message);
+    }
+}
+
+/// Logs the provided message without checking whether the global verbosity level
+/// allows it.
+fn log_unchecked(message: &Message) {
+    let Message { message, verbosity } = message;
+
+    let (prefix, suffix) = match verbosity {
+        Trace => ("   \x1B[1;2mtrace\x1B[0m\x1B[2m  ", "\x1B[0m"),
+        Info => ("    \x1B[1;92minfo\x1B[0m  ", ""),
+        Debug => ("   \x1B[1;95mdebug\x1B[0m  ", ""),
+        Warning => (" \x1B[1;93mwarning\x1B[0m\x1B[93m  ", "\x1B[0m"),
+        Error => ("   \x1B[1;31merror\x1B[0m\x1B[91m  ", "\x1B[0m"),
+    };
+
+    ft::printf!("{prefix}{message}{suffix}\n");
+}
+
+/// Logs a message with the [`Trace`] verbosity level.
+#[macro_export]
+macro_rules! trace {
+    ($($args:tt)*) => {
+        $crate::log(&$crate::message!($crate::Verbosity::Trace, $($args)*));
+    };
+}
+
+/// Logs a message with the [`Info`] verbosity level.
+#[macro_export]
+macro_rules! info {
+    ($($args:tt)*) => {
+        $crate::log(&$crate::message!($crate::Verbosity::Info, $($args)*));
+    };
+}
+
+/// Logs a message with the [`Debug`] verbosity level.
+#[macro_export]
+macro_rules! debug {
+    ($($args:tt)*) => {
+        $crate::log(&$crate::message!($crate::Verbosity::Debug, $($args)*));
+    };
+}
+
+/// Logs a message with the [`Warning`] verbosity level.
+#[macro_export]
+macro_rules! warning {
+    ($($args:tt)*) => {
+        $crate::log(&$crate::message!($crate::Verbosity::Warning, $($args)*));
+    };
+}
+
+/// Logs a message with the [`Error`] verbosity level.
+#[macro_export]
+macro_rules! error {
+    ($($args:tt)*) => {
+        $crate::log(&$crate::message!($crate::Verbosity::Error, $($args)*));
+    };
+}
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -9,7 +9,7 @@ fn main(_args: &[&ft::CharStar], _env: &[&ft::CharStar]) -> u8 {
     ft_async::EXECUTOR.spawn(run_server(1234));
 
     let err = ft_async::EXECUTOR.run();
-    ft::printf!("\x1B[1;31merror\x1B[0m: failed to run the executor: {err}\n");
+    ft_log::error!("failed to run the executor: {err}");
 
     0
 }
@@ -18,12 +18,12 @@ ft::entry_point!(main);
 
 /// Runs the server on the provided port.
 async fn run_server(port: u16) {
-    ft::printf!("\x1B[1;32minfo\x1B[0m: starting up the server on port {port}\n");
+    ft_log::info!("starting up the server on port {port}");
 
     let server = match Server::new(port) {
         Ok(ok) => ok,
         Err(err) => {
-            ft::printf!("\x1B[1;31merror\x1B[0m: failed to create a TCP server: {err}");
+            ft_log::error!("failed to create a TCP server: {err}");
             return;
         }
     };
@@ -32,14 +32,14 @@ async fn run_server(port: u16) {
         let conn = match server.accept().await {
             Ok(ok) => ok,
             Err(err) => {
-                ft::printf!("\x1B[1;31merror\x1B[0m: failed to accept a connection: {err}");
+                ft_log::error!("failed to accept a connection: {err}");
                 continue;
             }
         };
 
-        ft::printf!(
-            "\x1B[1;32minfo\x1B[0m: accepted a connection from `\x1B[33m{addr}\x1B[0m`\n",
-            addr = conn.address(),
+        ft_log::info!(
+            "accepted a connection from `\x1B[33m{}\x1B[0m`",
+            conn.address(),
         );
 
         ft_async::EXECUTOR.spawn(handle_connection(conn));
@@ -49,8 +49,8 @@ async fn run_server(port: u16) {
 /// Handles a connection from a client.
 async fn handle_connection(conn: Connection) {
     if let Err(err) = conn.send(b"Hello!\n").await {
-        ft::printf!(
-            "\x1B[1;31merror\x1B[0m: failed to send a message to `\x1B[33m{addr}\x1B[0m`: {err}\n",
+        ft_log::error!(
+            "failed to send a message to `\x1B[33m{addr}\x1B[0m`: {err}",
             addr = conn.address(),
         );
     }
