7205a0bc9b342106c244388bb13b4ebcffd0b5ec
ragarnoy <tiagolernould@gmail.com>
2023-11-23 00:13:14 +0100

    Add popup and grid features to App

--- crates/gfx/src/app.rs
+++ crates/gfx/src/app.rs
@@ -3,16 +3,58 @@
 pub struct App {
     /// should the application exit?
     pub should_quit: bool,
-    /// counter
-    pub counter: u8,
+    pub active_tab: usize,
+    pub grid: Vec<Vec<char>>,
+    pub selected_position: (usize, usize),
+    pub popup: Option<PopupState>,
 }
 
+#[derive(Debug, Default)]
+pub struct PopupState {
+    pub selected: usize,
+    pub command: PopupCommand,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Default)]
+pub enum PopupCommand {
+    #[default]
+    None,
+    Command1,
+    Command2,
+    Command3,
+}
+
+impl From<usize> for PopupCommand {
+    fn from(index: usize) -> Self {
+        match index {
+            0 => PopupCommand::Command1,
+            1 => PopupCommand::Command2,
+            2 => PopupCommand::Command3,
+            _ => PopupCommand::None,
+        }
+    }
+}
+
+impl From<PopupCommand> for PopupState {
+    fn from(command: PopupCommand) -> Self {
+        PopupState {
+            selected: 0,
+            command,
+        }
+    }
+}
 
 impl App {
-    /// Constructs a new instance of [`App`].
-    pub fn new() -> Self {
-        Self::default()
-    }
+
+    pub fn new() -> App {
+        let grid_size = 10; // Example size
+        let grid = vec![vec![' '; grid_size]; grid_size];
+
+        App {
+            grid,
+            ..Default::default()
+        }
+    }    /// Constructs a new instance of [`App`].
 
     /// Handles the tick event of the terminal.
     pub fn tick(&self) {}
@@ -22,15 +64,4 @@ impl App {
         self.should_quit = true;
     }
 
-    pub fn increment_counter(&mut self) {
-        if let Some(res) = self.counter.checked_add(1) {
-            self.counter = res;
-        }
-    }
-
-    pub fn decrement_counter(&mut self) {
-        if let Some(res) = self.counter.checked_sub(1) {
-            self.counter = res;
-        }
-    }
 }
