d95aee995bfff615f31f6f30314822fc5cbca368
nils-mathieu <nils@astralforge.art>
2025-08-29 14:15:10 +0200

    update workflow

--- crates/server/src/state/mod.rs
+++ crates/server/src/state/mod.rs
@@ -95,7 +95,9 @@ impl Response {
         match self {
             Response::Ok => ft_async::futures::write_all(fd, b"ok\n").await?,
             Response::ConnectNbr(nbr) => {
-                writeln!(buf, "{}", nbr).unwrap();
+                // NOTE: This cannot fail because writing to a string in this way will panic in case
+                // of memory allocation failure instead of returning an error.
+                _ = writeln!(buf, "{}", nbr);
                 ft_async::futures::write_all(fd, buf.as_bytes()).await?
             }
         }
