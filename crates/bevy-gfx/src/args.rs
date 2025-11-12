3f08bd201752bafcdc005f903a2afb880be51474
ygaude <ygaude@student.42.fr>
2025-11-12 12:31:12 +0100

    Add doc comments in args

--- crates/bevy-gfx/src/args.rs
+++ crates/bevy-gfx/src/args.rs
@@ -4,13 +4,15 @@ use clap::Parser;
 #[derive(Parser)]
 #[command(name = "Zappy GFX Client")]
 pub struct Cli {
-    /// Server address in the format host:port
+    /// Server address
     #[clap(short, long, default_value = "localhost")]
     pub server_address: String,
+    /// Server port
     #[clap(short, long, default_value = "1234")]
     pub port: u16,
 }
 
+/// Get the server address in "address:port" format
 pub fn server_address() -> String {
     let cli = Cli::parse();
     format!("{}:{}", cli.server_address, cli.port)
